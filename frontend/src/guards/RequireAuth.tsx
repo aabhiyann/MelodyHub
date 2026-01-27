6fe46821968e9a00de50b894f594fa5dac784043
aabhiyann <sainjuabhiyan321@gmail.com>
2026-01-26 21:20:44 -0500

    feat(social): Implement SocketManager and integrate in RequireAuth

--- frontend/src/guards/RequireAuth.tsx
+++ frontend/src/guards/RequireAuth.tsx
@@ -8,6 +8,7 @@ import { Navigate, useLocation } from 'react-router-dom';
 import { LoadingScreen } from '@/components/LoadingScreen';
 
 import { useAuthStore } from "@/stores/AuthStore";
+import { SocketManager } from "@/components/SocketManager";
 import { useEffect } from "react";
 import { axiosInstance } from "@/lib/axios";
 
@@ -50,5 +51,10 @@ export const RequireAuth = ({ children }: RequireAuthProps) => {
         return <Navigate to="/" state={{ from: location }} replace />;
     }
 
-    return <>{children}</>;
+    return (
+        <>
+            <SocketManager />
+            {children}
+        </>
+    );
 };
